ef22d00823d0a5159f32244b5feab997e00b0ee6
Dương Thị Thương <thuongdthe150682@fpt.edu.vn>
2022-03-07 23:59:31 +0700

    database

--- PRJ_Project/src/java/DAO/AccountDAO.java
+++ PRJ_Project/src/java/DAO/AccountDAO.java
@@ -65,9 +65,9 @@ public class AccountDAO extends BaseDAO{
         }
         return check;
     }
-    public static void main(String[] args) {
-        AccountDAO dao = new AccountDAO();
-        System.out.println(dao.getAnAccount("hoanganh", "123@123"));
-    }
+//    public static void main(String[] args) {
+//        AccountDAO dao = new AccountDAO();
+//        System.out.println(dao.getAllAccount().size());
+//    }
      
 }
